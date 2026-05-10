638f9a1b8c8446c7ce684af642e5374e4bfff8a8
Pablo sebastian reyes revilla <ps.reyes@duocuc.cl>
2026-05-10 16:24:37 -0400

    feat: implementar servicio JWT para generacion de tokens

---
+++ Microservicio-Usuarios/src/main/java/com/donaton/usuarios/security/JwtService.java
@@ -0,0 +1,23 @@
+package com.donaton.usuarios.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private final String SECRET_KEY = "donaton_secret_key";
+
+    public String generarToken(String correo) {
+
+        return Jwts.builder()
+                .setSubject(correo)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+}
