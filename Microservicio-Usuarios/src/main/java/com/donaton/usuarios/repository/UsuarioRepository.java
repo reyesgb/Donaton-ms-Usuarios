7827db99b27ce165cda3846994e59075e15972d5
Pablo sebastian reyes revilla <ps.reyes@duocuc.cl>
2026-05-10 15:25:19 -0400

    feat: implementar repository pattern para usuarios

---
+++ Microservicio-Usuarios/src/main/java/com/donaton/usuarios/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package com.donaton.usuarios.repository;
+
+import com.donaton.usuarios.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
