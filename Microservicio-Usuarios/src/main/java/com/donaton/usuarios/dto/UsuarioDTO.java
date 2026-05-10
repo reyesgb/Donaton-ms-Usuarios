c56e77db12a97719ea2209ff2fd51dccd744f46e
Pablo sebastian reyes revilla <ps.reyes@duocuc.cl>
2026-05-10 14:43:18 -0400

    feat: implementar DTO y validaciones para usuarios

---
+++ Microservicio-Usuarios/src/main/java/com/donaton/usuarios/dto/UsuarioDTO.java
@@ -0,0 +1,18 @@
+package com.donaton.usuarios.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class UsuarioDTO {
+
+    @NotBlank
+    private String nombre;
+
+    @Email
+    private String correo;
+
+    @NotBlank
+    private String rol;
+}
