528406d5f1e6f38e8c837f1ac41626913c305502
Pablo sebastian reyes revilla <ps.reyes@duocuc.cl>
2026-05-10 14:36:48 -0400

    feat: crear entidad Usuario

---
+++ Microservicio-Usuarios/src/main/java/com/donaton/usuarios/model/Usuario.java
@@ -0,0 +1,22 @@
+package com.donaton.usuarios.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String correo;
+
+    private String rol;
+}
