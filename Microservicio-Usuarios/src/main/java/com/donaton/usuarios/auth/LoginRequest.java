5fae1891859d7735539d7c0ba54b3c525ee7b3c9
Pablo sebastian reyes revilla <ps.reyes@duocuc.cl>
2026-05-10 16:39:27 -0400

    feat: agregar DTOs para autenticacion JWT

---
+++ Microservicio-Usuarios/src/main/java/com/donaton/usuarios/auth/LoginRequest.java
@@ -0,0 +1,10 @@
+package com.donaton.usuarios.auth;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+
+    private String correo;
+    private String password;
+}
---
+++ Microservicio-Usuarios/src/main/java/com/donaton/usuarios/auth/LoginResponse.java
@@ -0,0 +1,11 @@
+package com.donaton.usuarios.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponse {
+
+    private String token;
+}
