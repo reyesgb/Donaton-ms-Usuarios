149437fe3bde6ab7aaaf89da5c456284954626d6
Pablo sebastian reyes revilla <ps.reyes@duocuc.cl>
2026-05-10 20:48:17 -0400

    fix: corregir generacion de token JWT compatible con jjwt

--- Microservicio-Usuarios/src/main/java/com/donaton/usuarios/security/JwtService.java
+++ Microservicio-Usuarios/src/main/java/com/donaton/usuarios/security/JwtService.java
@@ -2,22 +2,27 @@ package com.donaton.usuarios.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Service
 public class JwtService {
 
-    private final String SECRET_KEY = "donaton_secret_key";
+    private final SecretKey SECRET_KEY =
+            Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
     public String generarToken(String correo) {
 
         return Jwts.builder()
                 .setSubject(correo)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .setExpiration(
+                        new Date(System.currentTimeMillis() + 1000 * 60 * 60)
+                )
+                .signWith(SECRET_KEY)
                 .compact();
     }
 }
