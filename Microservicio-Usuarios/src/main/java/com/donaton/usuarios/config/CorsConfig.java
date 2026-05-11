9273b3ce6267f9a68ed43a1798db8317bcacf093
Pablo sebastian reyes revilla <ps.reyes@duocuc.cl>
2026-05-10 22:58:38 -0400

    feat: agregar busqueda de usuario por correo

--- Microservicio-Usuarios/src/main/java/com/donaton/usuarios/config/CorsConfig.java
+++ Microservicio-Usuarios/src/main/java/com/donaton/usuarios/config/CorsConfig.java
@@ -1,4 +1,31 @@
 package com.donaton.usuarios.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.*;
+
+
+@Configuration
 public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("http://localhost:5173");
+
+        configuration.addAllowedMethod("*");
+
+        configuration.addAllowedHeader("*");
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 }
--- Microservicio-Usuarios/src/main/java/com/donaton/usuarios/repository/UsuarioRepository.java
+++ Microservicio-Usuarios/src/main/java/com/donaton/usuarios/repository/UsuarioRepository.java
@@ -2,6 +2,8 @@ package com.donaton.usuarios.repository;
 
 import com.donaton.usuarios.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByCorreo(String correo);
 }
